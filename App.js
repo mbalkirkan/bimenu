c983e11376c3a4c101a9c64b2db3d541b8beb729
Muhammet <hbmemati@gmail.com>
2019-06-19 17:21:12 +0300

    bimenu 1

--- App.js
+++ App.js
@@ -6,80 +6,98 @@ import {
   Text,
   View,
 } from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+import BarcodeScannerExample from './src/pages/Barcode';
+
+
+const AppNavigator = createStackNavigator({
+    BarcodePage: {
+        screen: BarcodeScannerExample
+    }
+});
+
+export default createAppContainer(AppNavigator);
+
+
+
 
 // This refers to the function defined earlier in this guide
-import { Permissions } from 'expo';
+// import { Permissions } from 'expo';
+//
+// const PUSH_ENDPOINT = 'https://your-server.com/users/push-token';
+//
+// async function registerForPushNotificationsAsync() {
+//   const { status: existingStatus } = await Permissions.getAsync(
+//       Permissions.NOTIFICATIONS
+//   );
+//   let finalStatus = existingStatus;
+//
+//   // only ask if permissions have not already been determined, because
+//   // iOS won't necessarily prompt the user a second time.
+//   if (existingStatus !== 'granted') {
+//     // Android remote notification permissions are granted during the app
+//     // install, so this will only ask on iOS
+//     const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+//     finalStatus = status;
+//   }
+//
+//   // Stop here if the user did not grant permissions
+//   if (finalStatus !== 'granted') {
+//     return;
+//   }
+//
+//   // Get the token that uniquely identifies this device
+//   let token = await Notifications.getExpoPushTokenAsync();
+//
+//   console.log(token);
+//
+//   // POST the token to your backend server from where you can retrieve it to send push notifications.
+//   return fetch(PUSH_ENDPOINT, {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       token: {
+//         value: token,
+//       },
+//       user: {
+//         username: 'Brent',
+//       },
+//     }),
+//   });
+// }
 
-const PUSH_ENDPOINT = 'https://your-server.com/users/push-token';
 
-async function registerForPushNotificationsAsync() {
-  const { status: existingStatus } = await Permissions.getAsync(
-      Permissions.NOTIFICATIONS
-  );
-  let finalStatus = existingStatus;
+// export default class AppContainer extends React.Component {
+//   state = {
+//     notification: '',
+//     deneme:''
+//   };
+//
+//   componentDidMount() {
+//     registerForPushNotificationsAsync();
+//
+//     // Handle notifications that are received or selected while the app
+//     // is open. If the app was closed and then opened by tapping the
+//     // notification (rather than just tapping the app icon to open it),
+//     // this function will fire on the next tick after the app starts
+//     // with the notification data.
+//     this._notificationSubscription = Notifications.addListener(this._handleNotification);
+//   }
+//
+//   _handleNotification = (notification) => {
+//     this.setState({notification: notification});
+//   };
 
-  // only ask if permissions have not already been determined, because
-  // iOS won't necessarily prompt the user a second time.
-  if (existingStatus !== 'granted') {
-    // Android remote notification permissions are granted during the app
-    // install, so this will only ask on iOS
-    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-    finalStatus = status;
-  }
-
-  // Stop here if the user did not grant permissions
-  if (finalStatus !== 'granted') {
-    return;
-  }
-
-  // Get the token that uniquely identifies this device
-  let token = await Notifications.getExpoPushTokenAsync();
-
-  console.log(token);
-
-  // POST the token to your backend server from where you can retrieve it to send push notifications.
-  return fetch(PUSH_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      token: {
-        value: token,
-      },
-      user: {
-        username: 'Brent',
-      },
-    }),
-  });
-}
-export default class AppContainer extends React.Component {
-  state = {
-    notification: '',
-  };
-
-  componentDidMount() {
-    registerForPushNotificationsAsync();
-
-    // Handle notifications that are received or selected while the app
-    // is open. If the app was closed and then opened by tapping the
-    // notification (rather than just tapping the app icon to open it),
-    // this function will fire on the next tick after the app starts
-    // with the notification data.
-    this._notificationSubscription = Notifications.addListener(this._handleNotification);
-  }
-
-  _handleNotification = (notification) => {
-    this.setState({notification: notification});
-  };
-
-  render() {
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Text>Origin: {this.state.notification}</Text>
-          <Text>Data: {JSON.stringify(this.state.notification)}</Text>
-        </View>
-    );
-  }
-}
+//   render() {
+//     return (
+//         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+//           <Text>Origin: {this.state.notification}</Text>
+//           <Text>Data: {JSON.stringify(this.state.notification)}</Text>
+//         </View>
+//     );
+//   }
+// }
